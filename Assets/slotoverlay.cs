6c13d17a622161bb4e7d8891f390c1b2c18429e1
DaaseAllan <laurits.mumberg@gmail.com>
2017-07-07 14:15:35 +0200

    mere gamble

---
+++ Assets/slotoverlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slotoverlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// get all renderers in this object and its children:
+		Renderer.material.renderQueue = 2002;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
